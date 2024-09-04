07f6837b42b8fa908deada0bfac2626ca22987ec
dayanalm <jdlugom@libertadores.edu.co>
2024-09-04 03:24:52 +0000

    Pending changes exported from your codespace

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function setup() {
-    createCanvas(400, 400);
-    background(220);
-  }
-  
-  function draw() {
-    ellipse(mouseX, mouseY, 50, 50);
-  }
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  ellipse(mouseX, mouseY, 50, 50);
+}
